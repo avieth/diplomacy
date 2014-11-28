98adc76549298548e91ca992129f8083e79bdee5
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2014-11-27 20:08:52 -0500

    Define Turn

---
+++ Diplomacy/Turn.hs
@@ -0,0 +1,23 @@
+module Turn (
+
+    Turn
+
+  , firstTurn
+  , nextTurn
+
+  ) where
+
+newtype Turn = Turn Int
+  deriving (Eq, Ord)
+
+instance Show Turn where
+  show = show . asYear
+
+firstTurn :: Turn
+firstTurn = Turn 1
+
+nextTurn :: Turn -> Turn
+nextTurn (Turn i) = Turn (i+1)
+
+asYear :: Turn -> Int
+asYear (Turn n) = 1900 + n
