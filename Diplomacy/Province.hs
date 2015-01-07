a2b3e2cbffc4854ddca65952a174fe435426bc47
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-01-06 20:11:41 -0500

    Province.hs describes move, convoy adjacency
    Takes into consideration the special coastline cases.
    
    Also remove the useless SupplyCentre = Bool type

--- Diplomacy/Province.hs
+++ Diplomacy/Province.hs
@@ -6,6 +6,9 @@ module Diplomacy.Province (
   , adjacency
   , adjacent
 
+  , convoyAdjacent
+  , moveAdjacent
+
   , ProvinceType(..)
   , provinceType
   , supplyCentre
@@ -24,6 +27,9 @@ module Diplomacy.Province (
 
   , ProvinceTarget(..)
   , ProvinceCoast(..)
+
+  , isNormal
+  , isSpecial
 
   , pcProvince 
   , provinceCoasts
@@ -309,9 +315,7 @@ antireflexivityCheck = filter (not . adjacencyIsAntireflexive) allProvinces
 adjacencyIsAntireflexive :: Province -> Bool
 adjacencyIsAntireflexive p = not $ p `adjacent` p
 
-type SupplyCentre = Bool
-
-supplyCentre :: Province -> SupplyCentre
+supplyCentre :: Province -> Bool
 supplyCentre Norway = True
 supplyCentre Sweden = True
 supplyCentre Denmark = True
@@ -441,6 +445,14 @@ data ProvinceTarget
   | Special ProvinceCoast
     deriving (Eq, Ord, Show)
 
+isSpecial :: ProvinceTarget -> Bool
+isSpecial (Special _) = True
+isSpecial _ = False
+
+isNormal :: ProvinceTarget -> Bool
+isNormal (Normal _) = True
+isNormal _ = False
+
 allProvinceTargets = map Normal allProvinces ++ map Special (allProvinces >>= provinceCoasts)
 
 -- | Like allProvinceTargets but those ProvinceTargets which have ProvinceCoasts
@@ -499,3 +511,58 @@ isWater :: Province -> Bool
 isWater prv = case provinceType prv of
   Water -> True
   _ -> False
+
+-- | Valid moves do not correspond exactly to adjacent (incl. convoy) provinces.
+--   We account here for the special cases involving coastlines.
+--   See https://www.wizards.com/avalonhill/rules/diplomacy.pdf page 5
+--   The Kiel/Constantinople and Sweden/Denmark clarifications are already
+--   defined in the Province adjacency; we need only look after the split
+--   coastlines in StPetersburg, Bulgaria, and Spain.
+moveAdjacent :: ProvinceTarget -> ProvinceTarget -> Bool
+-- Easiest case first: convince yourself that special coastline is not
+-- reachable from a special coastline. This is the whole point of special
+-- coastlines: a unit on one can never move immediately to another; it must
+-- go through water provinces.
+moveAdjacent (Special _) (Special _) = False
+-- A special province target is never reached by convoy, so we can use adjacent
+-- to determine this. A move to a special target must be from a non-inland
+-- province target (subtle: only fleets can move to special coastlines, and
+-- fleets will never be inland), and we use the blacklist function to take
+-- care of ad-hoc special cases presented in the rulebook.
+moveAdjacent pt0 (Special pc) =
+    let prv0 = ptProvince pt0
+    in (not (blacklist prv0 pc)) && (not (isInland prv0)) && ((pcProvince pc) `adjacent` prv0)
+-- Surely reachability is symmetric!
+moveAdjacent (Special pc) pt0 = moveAdjacent pt0 (Special pc)
+-- The fall-through case: just use the Province adjacency definition, eliminating
+-- a convoy-adjacent move for fleets.
+moveAdjacent (Normal prv0) (Normal prv1) = prv0 `adjacent` prv1
+
+convoyAdjacent :: ProvinceTarget -> ProvinceTarget -> Bool
+convoyAdjacent (Special _) _ = False
+convoyAdjacent _ (Special _) = False
+convoyAdjacent (Normal prv0) (Normal prv1) = convoyAdjacent' prv0 prv1
+
+-- | Adjacent via convoy iff there is a nontrivial noncyclic path over water
+--   provinces from first province to second.
+convoyAdjacent' :: Province -> Province -> Bool
+convoyAdjacent' prv0 prv1
+    | prv0 == prv1 = False
+    | otherwise = convoyAdjacent'' [] prv0 prv1
+  where
+    -- Must use the 'used' name to track provinces already visited, so that we don't
+    -- get caught in loops in our adjacency function.
+    convoyAdjacent'' used prv0 prv1 =
+      let waterNeighbours = filter (\x -> isWater x && (not (elem x used))) (adjacency prv0)
+      in any (\x -> (x `adjacent` prv1) || (convoyAdjacent'' (x : used) x prv1)) waterNeighbours
+
+-- | True iff the given province should not be considered adjacent to the
+--   given province coast.
+blacklist :: Province -> ProvinceCoast -> Bool
+blacklist WesternMediterranean SpainNorth = True
+-- NB MidAtlanticOcean to SpainSouth is fine!
+blacklist GulfOfBothnia StPetersburgNorth = True
+blacklist BarentsSea StPetersburgWest = True
+blacklist BlackSea BulgariaSouth = True
+blacklist AegeanSea BulgariaEast = True
+blacklist _ _ = False
