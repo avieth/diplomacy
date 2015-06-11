5767553049e7c9b06ec8f15c5b2f05996700a004
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-06-11 18:41:21 -0400

    runMayFail defined through the Compose

--- Data/MayFail.hs
+++ Data/MayFail.hs
@@ -22,6 +22,8 @@ module Data.MayFail (
 
   , hoist
 
+  , runMayFail
+
   ) where
 
 import Control.Applicative
@@ -30,7 +32,7 @@ import Data.Functor.Compose
 import Data.Functor.Identity
 
 newtype MayFail e f t = MayFail {
-    runMayFail :: Compose (Either e) f t
+    getMayFail :: Compose (Either e) f t
   } deriving (Functor, Applicative)
 
 instance Alternative f => Alternative (MayFail e f) where
@@ -40,8 +42,8 @@ instance Alternative f => Alternative (MayFail e f) where
         (_, Left e) -> Left e
         (Right x, Right y) -> Right (x <|> y)
       where
-        x' = getCompose . runMayFail $ x
-        y' = getCompose . runMayFail $ y
+        x' = getCompose . getMayFail $ x
+        y' = getCompose . getMayFail $ y
 
 class CommutesWithEither f where
     commuteEither :: f (Either e a) -> Either e (f a)
@@ -51,9 +53,9 @@ instance CommutesWithEither Identity where
 
 instance (Functor f, Monad f, CommutesWithEither f) => Monad (MayFail e f) where
     return = MayFail . Compose . return . return
-    x >>= k = MayFail . Compose $ case getCompose . runMayFail $ x of
+    x >>= k = MayFail . Compose $ case getCompose . getMayFail $ x of
         Left e -> Left e
-        Right fterm -> case commuteEither . fmap (getCompose . runMayFail . k) $ fterm of
+        Right fterm -> case commuteEither . fmap (getCompose . getMayFail . k) $ fterm of
             Left e' -> Left e'
             Right x -> Right (join x)
 
@@ -65,3 +67,6 @@ passes = MayFail . Compose . Right
 
 hoist :: (f t -> g s) -> MayFail e f t -> MayFail e g s
 hoist trans (MayFail (Compose x)) = MayFail (Compose (fmap trans x))
+
+runMayFail :: MayFail e f t -> Either e (f t)
+runMayFail = getCompose . getMayFail
