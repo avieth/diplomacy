458051ada41e4a986158facd74de3b11fa2660fa
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-06-11 18:41:05 -0400

    Many is Foldable, new functions

--- Data/Many.hs
+++ Data/Many.hs
@@ -14,6 +14,7 @@ module Data.Many (
 
     Many(..)
 
+  , makeMany
   , manyCons
   , manySnoc
   , manyAppend
@@ -25,19 +26,25 @@ module Data.Many (
   , SplitMany(..)
   , splitMany
 
+  , popMany
+
   ) where
+
+import Prelude hiding (foldr)
+import Data.Foldable
 
 data Many t = One t | Many t (Many t)
 
 instance Show t => Show (Many t) where
-    show xs = case xs of
-        One x -> show x
-        Many x xs -> show x ++ ", " ++ show xs
+    show = show . manyToList
 
 instance Functor Many where
     fmap f xs = case xs of
         One x -> One (f x)
         Many x xs -> Many (f x) (fmap f xs)
+
+instance Foldable Many where
+    foldr f b = foldr f b . manyToList
 
 manyCons :: t -> Many t -> Many t
 manyCons = Many
@@ -72,6 +79,11 @@ manyToList xs = case xs of
     One x -> [x]
     Many x xs' -> x : manyToList xs'
 
+makeMany :: t -> [t] -> Many t
+makeMany x xs = case xs of
+    [] -> One x
+    x' : xs' -> Many x (makeMany x' xs')
+
 data SplitMany t
     = SplitNotPresent -- Splitter was not present.
     | SplitEmpty -- Splitter was the only element.
@@ -88,3 +100,8 @@ splitMany x xs = case xs of
     splitManyRec x xs left = case xs of
         One x' -> if x' == x then SplitEnd left else SplitNotPresent
         Many x' xs' -> if x' == x then SplitMiddle left xs' else splitManyRec x xs' (manySnoc left x')
+
+popMany :: Many t -> (t, Maybe (Many t))
+popMany xs = case xs of
+    One x -> (x, Nothing)
+    Many x xs -> (x, Just xs)
