f1f9e996e777005eafd881accb3bd7e589820406
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-07-12 14:51:12 -0400

    Define Control.hs

---
+++ Diplomacy/Control.hs
@@ -0,0 +1,38 @@
+{-|
+Module      : Diplomacy.Control
+Description : Definition of control of provinces.
+Copyright   : (c) Alexander Vieth, 2015
+Licence     : BSD3
+Maintainer  : aovieth@gmail.com
+Stability   : experimental
+Portability : non-portable (GHC only)
+-}
+
+{-# LANGUAGE AutoDeriveTypeable #-}
+
+module Diplomacy.Control (
+
+    Control
+
+  , emptyControl
+  , control
+  , controller
+
+  ) where
+
+import qualified Data.Map as M
+import Diplomacy.Province
+import Diplomacy.GreatPower
+
+-- | Indicates which GreatPower most recently had a unit on a given Province
+--   at the beginning of an adjust phase.
+type Control = M.Map Province GreatPower
+
+emptyControl :: Control
+emptyControl = M.empty
+
+control :: Province -> Maybe GreatPower -> Control -> Control
+control pr mgp = M.alter (const mgp) pr
+
+controller :: Province -> Control -> Maybe GreatPower
+controller = M.lookup
